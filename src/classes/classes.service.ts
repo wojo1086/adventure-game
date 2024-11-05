43defd0a1fb59b3b169f1b8e347ab5ef936d0dbf
timothywojtylak <t.wojtylak@gmail.com>
2024-11-05 18:42:54 -0500

    Adding user service

--- src/classes/classes.service.ts
+++ src/classes/classes.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CreateClassDto } from './dto/create-class.dto';
 import { UpdateClassDto } from './dto/update-class.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Class } from './entities/class.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ClassesService {
+
+    constructor(@InjectRepository(Class) private readonly classRepository: Repository<Class>,) {
+    }
   create(createClassDto: CreateClassDto) {
     return 'This action adds a new class';
   }
 
   findAll() {
-    return `This action returns all classes`;
+    return this.classRepository.find();
   }
 
   findOne(id: number) {
--- src/classes/entities/class.entity.ts
+++ src/classes/entities/class.entity.ts
@@ -10,7 +10,7 @@ export class Class {
 
     @Column({
         type: "varchar",
-        length: 300
+        length: 500
     })
     description: string;
 }
